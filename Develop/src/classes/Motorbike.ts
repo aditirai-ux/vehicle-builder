fc99a5e879f6e87f895577dbfaddd0fad998eb18
Aditi Rai <aditi45@gmail.com>
2024-09-24 17:24:21 -0400

    finalization of motorbike class

--- Develop/src/classes/Motorbike.ts
+++ Develop/src/classes/Motorbike.ts
@@ -17,7 +17,9 @@ class Motorbike extends Vehicle{
   // TODO: Create a constructor that accepts the properties of the Motorbike class
   // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[])
   constructor(vin: string, color: string, make: string, model: string, year: number, weight: number, topSpeed: number, wheels: Wheel[]){
+   // TODO: The constructor should call the constructor of the parent class, Vehicle
     super();
+    // TODO: The constructor should initialize the properties of the Motorbike class
     this.vin = vin;
     this.color = color;
     this.make = make;
@@ -29,11 +31,7 @@ class Motorbike extends Vehicle{
     if(this.wheels.length !== 2){
       this.wheels = [new Wheel(), new Wheel()]; // TODO: The constructor should check if the wheels array has 2 elements and create 2 new default Wheel objects if it does not
     }
-  }
-   
-    // TODO: The constructor should call the constructor of the parent class, Vehicle
-    // TODO: The constructor should initialize the properties of the Motorbike class
-    
+  }   
 
   // TODO: Implement the wheelie method
     // TODO: The method should log the message "Motorbike [make] [model] is doing a wheelie!"
