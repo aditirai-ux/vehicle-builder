66da58f5a64d31e7f459b8a39664bd3b11dad646
Aditi Rai <aditi45@gmail.com>
2024-09-24 17:15:19 -0400

    added properties and constructor to motorbike class

--- Develop/src/classes/Motorbike.ts
+++ Develop/src/classes/Motorbike.ts
@@ -3,9 +3,31 @@ import Vehicle from './Vehicle.js';
 import Wheel from './Wheel.js';
 
 // TODO: The Motorbike class should extend the Vehicle class
-class Motorbike {
-  // TODO: Declare properties of the Motorbike class
-  // TODO: The properties should include vin, color, make, model, year, weight, top speed, and wheels
+// TODO: Declare properties of the Motorbike class
+class Motorbike extends Vehicle{
+  vin: string;
+  color: string; 
+  make: string; 
+  model: string; 
+  year: number; 
+  weight: number; 
+  topSpeed: number; 
+  wheels: Wheel[];
+  
+  constructor(vin: string, color: string, make: string, model: string, year: number, weight: number, topSpeed: number, wheels: Wheel[]){
+    super();
+    this.vin = vin;
+    this.color = color;
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.weight = weight;
+    this.topSpeed = topSpeed;
+    this.wheels = wheels;
+    if(this.wheels.length !== 2){
+      this.wheels = [new Wheel(), new Wheel()];
+    }
+  }
   // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[])
 
   // TODO: Create a constructor that accepts the properties of the Motorbike class
